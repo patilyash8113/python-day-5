3eb6f2ec36604640aa6b94ddb3be2a608d6312ed
patilyash8113 <yashpatil8113@gmail.com>
2025-12-31 11:32:42 +0530

    Add files via upload

---
+++ 05_comments_print_ESC.py/05_esc.py
@@ -0,0 +1,11 @@
+print("how are you \ni \ am good")
+
+print("hello \" world")
+
+print("hello \nworld")
+print("this is a tab\t character")
+
+print("this is a \\yash patil")
+
+print("yash patil \"at\" chane")
+
---
+++ 05_comments_print_ESC.py/05_print.py
@@ -0,0 +1,3 @@
+print("hi are","you there",'yash',5,sep='/')
+print("how are you",end="?")
+print("yash",end="///")
